53ce9e98b1435b7cf817c6897fb78b7d513ccd86
JrGustavo <gus@soypetal.com>
2023-01-04 22:10:26 -0500

    First class Java Master

---
+++ src/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        
+    String saludar = " Hola mundo desde Java";
+        System.out.print(saludar);
+        System.out.println("saludar = " + saludar);    
+
+    }
+
+
+
+}
